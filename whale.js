8f9be1359f75fb4cc1ac90ad6027945b9793daca
Zian Zhou <61924332+ZianZhou@users.noreply.github.com>
2021-07-03 14:49:42 -0700

    Create whale.js

---
+++ whale.js
@@ -0,0 +1,19 @@
+var input = "a whale of a deal!";
+
+var vowels = ['a', 'e','i','o','u'];
+
+let resultArray =[];
+
+for(let i=0;i<input.length;i++){
+  //console.log(i);
+  for(let j=0;j<vowels.length;j++){
+    //console.log(j);
+    if(input[i]===vowels[j]){
+      resultArray.push(input[i]);
+      if(input[i]==='e' ||input[i]==='u'){
+        resultArray.push(input[i]);
+      }
+    }
+  }
+}
+console.log(resultArray.join('').toUpperCase());
