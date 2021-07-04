22f2d9cd13976d3b7e9945da8bdfda0b3266c5d7
Zian Zhou <61924332+ZianZhou@users.noreply.github.com>
2021-07-04 14:26:18 -0700

    Create dog.js

---
+++ dog.js
@@ -0,0 +1,32 @@
+// Write your code here:
+const dogFactory=(name,breed,weight)=>{
+  return {
+    _name: name,
+    _breed: breed,
+    _weight: weight,
+    get name(){
+      return this._name;
+    },
+    get breed(){
+      return this._breed;
+    },
+    get weight(){
+      return this._weight;
+    } ,
+    set name(name){
+      this._name=name;
+    },
+    set breed(breed){
+      this._breed=breed;
+    },
+    set weight(weight){
+      this._weight=weight;
+    },
+    bark(){
+      return 'ruff! ruff!';
+    },
+    eatTooManyTreats(){
+      this._weight++;
+    }
+  }
+  }
